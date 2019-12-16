5e79723f07f207b86464033021981c22d750b551
chris-etn <chris.day@office.ad>
2019-12-16 16:38:20 +0000

    ANY-102 : Single space

--- Service/ProfanityChecker.php
+++ Service/ProfanityChecker.php
@@ -364,7 +364,7 @@ class ProfanityChecker
     public function clearSeparatorExpression(): self
     {
         // The second parameter cannot be empty
-        $this->separatorExpression  = $this->generateEscapedExpression([], ['¬']);
+        $this->separatorExpression = $this->generateEscapedExpression([], ['¬']);
 
         return $this;
     }
